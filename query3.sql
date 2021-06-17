6ef5a1536613942a2bfef9765f1f9d401283d641
Shanley Thomas <shanley_b190901me.nitc.ac.in>
2021-06-18 00:22:04 +0530

    query 3 change

--- query3.sql
+++ query3.sql
@@ -1,1 +1,1 @@
-SELECT b.title FROM books b , books_subjects bs WHERE bs.subject IN ('Politics','Technology') AND bs.book=b.id;
+SELECT b.title FROM books b , subjects s , books_subjects bs where b.id = bs.book and s.id = bs.subject AND s.name IN ( 'Technology','Politics'); 
